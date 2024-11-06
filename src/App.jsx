69b21cabaa247ceb00ee89a8a63b38bb891638c0
Moltish <moltotalm@gmail.com>
2024-11-05 21:24:44 -0500

    add_employee component created and set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './Markup/Pages/Home/Home'
-import Login from './Markup/Pages/Login/Login';
-import Employee from './Markup/Pages/Admin/Employee/Employee';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Markup/Pages/Home/Home";
+import Login from "./Markup/Pages/Login/Login";
+import Employee from "./Markup/Pages/Admin/Employee/Employee";
+import AddEmployeeForm from "./Markup/Components/Admin/AddEmployee/AddEmployeeForm";
 
 function App() {
- 
-
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/add-employee" element={<Employee />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/add-employee" element={<Employee />} />
+      </Routes>
+      <AddEmployeeForm />
+    </>
   );
 }
 
-export default App
+export default App;
